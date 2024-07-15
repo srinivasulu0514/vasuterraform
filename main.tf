9b3fcd19df7446fd7946e7cb56ba9fe27c9604a5
Srinivasulu Dammu <Dammu.10741834@LTIMindtree.com>
2024-07-15 14:42:46 +0530

    Added main.tf file

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = "ap-south-1" # setting desired location
+  }
+
+resource "aws_instance" "example" {
+  ami                     = "ami-0ec0e125bb6c6e8ec" # specify appropriate AMI ID
+  instance_type           = "t2.micro"
+  subnet_id = "subnet-0fbc5d4f7a772c702"
+}
