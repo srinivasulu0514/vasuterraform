347ca90d2e184c7e927712eb40c6a9e47501e0f9
Srinivasulu Dammu <Dammu.10741834@LTIMindtree.com>
2024-07-15 15:51:27 +0530

    Added main.tf file

--- main.tf
+++ main.tf
@@ -6,4 +6,6 @@ resource "aws_instance" "example" {
   ami                     = "ami-0ec0e125bb6c6e8ec" # specify appropriate AMI ID
   instance_type           = "t2.micro"
   subnet_id = "subnet-0fbc5d4f7a772c702"
+  key_name = "Jenkins_login"
+  name = "Terraform"
 }
